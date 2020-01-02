f603299bce7787e6e4c8c193efa0bbe1c49c3463
Simon Carrignon <simon@nimbios.org>
2020-01-02 10:11:58 -0500

    merge both plotAll funciton in one

--- tools.R
+++ tools.R
@@ -1,32 +1,22 @@
 
 
-plotAllVariable <- function(results){
-    allsd=sapply(results$allpop,function(i)apply(i[,3:9],2,sd))
-    allmean=sapply(results$allpop,function(i)apply(i[,3:9],2,mean))
-    cols=rainbow(nrow(allsd)+1)
+plotAllVariable <- function(results,hdr=F,vars=3:9){
+    allsd=sapply(results$allpop,function(i)apply(i[,vars],2,sd))
+    allmean=sapply(results$allpop,function(i)apply(i[,vars],2,mean))
+    cols=c(rainbow(nrow(allsd)),"black")
     names(cols)=c(rownames(allsd),"theta")
     par(mfrow=c(8,1),mar=c(1,4,1,1))
     lapply(rownames(allsd),function(varname)
            {
-               lims=range(c(allmean[varname,]+allsd[varname,],allmean[varname,]-allsd[varname,]))
-               plot(allmean[varname,],ylab=varname,type="l",col=cols[varname],ylim=lims,xaxt='n')
-               lines(allmean[varname,]+allsd[varname,],ylab=varname,col=cols[varname],lty=3)
-               lines(allmean[varname,]-allsd[varname,],ylab=varname,col=cols[varname],lty=3)
-           })
-    par(mar=c(2,4,0,1))
-    plot(results$env,type="l",col=cols["theta"],ylab="theta")
-}
-
-
-
-plotAllHDR <- function(results,var="w"){
-    allsd=sapply(results$allpop,function(i)apply(i[,3:9],2,sd))
-    cols=rainbow(nrow(allsd)+1)
-    par(mfrow=c(8,1),mar=c(1,4,1,1))
-    lapply(rownames(allsd),function(varname)
-           {
-               hdr.boxplot(lapply(results$allpop,"[[",varname),border=NA,pch=".",outline=F,col=c(cols[varname],"darkgreen"),space=0,ylab=varname)
-           })
+	
+			   if(hdr)hdr.boxplot(lapply(results$allpop,"[[",varname),border=NA,pch=".",outline=F,col=shades(cols[varname],3),prob=c(50,75,99),space=0,ylab=varname)
+			   else {
+				   lims=range(c(allmean[varname,]+allsd[varname,],allmean[varname,]-allsd[varname,]))
+				   plot(allmean[varname,],ylab=varname,type="l",col=cols[varname],ylim=lims,xaxt='n')
+				   lines(allmean[varname,]+allsd[varname,],ylab=varname,col=cols[varname],lty=3)
+				   lines(allmean[varname,]-allsd[varname,],ylab=varname,col=cols[varname],lty=3)
+			   }
+		   })
     par(mar=c(2,4,0,1))
     plot(results$env,type="l",col=cols["theta"],ylab="theta")
 }
