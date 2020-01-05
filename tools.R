7be4abdc2c30496498c1151d801712fc5ccfd16c
Simon Carrignon <simon@nimbios.org>
2020-01-05 17:47:00 -0500

    add wrapper funciton to run tests

--- tools.R
+++ tools.R
@@ -33,5 +33,42 @@ plotAllVariable <- function(results,hdr=F,vars=3:9,...){
     par(mar=c(2,4,0,1))
     plot(results$theta,type="l",col=cols["theta"],ylab="theta")
 }
+
+#wrappers for exploration
+replicateNTime <- function(repet,n,tstep,omega,delta,b,K,mu,E,sigma,pop,...){
+
+    do.call("rbind",replicate(repet,getVarXMeanW(n=n,tstep=tstep,omega = omega,delta = delta ,b=b,K=K,mu=mu,E=E,sigma=sigma,pop=pop),simplify=F))
 }
+
+
+getVarXMeanW <- function(n,tstep,omega,delta,b,K,mu,E,sigma,pop,...){
+    t=simpleEvoModel(n=n,tstep=tstep,omega = omega,delta = delta ,b=b,K=K,mu=mu,E=E,sigma=sigma,pop=pop,...)
+    if(is.null(t$sd$x[tstep]))
+        return(c("var(x)"=NA,"mean(w)"=NA))
+    else
+        return(c("var(x)"=t$sd$x[tstep],"mean(w)"=t$mean$w[tstep]))
+}
+
+writeResults <- function(final,var){
+    pdf(paste0("explore_",var,".pdf"),width=10)
+    par(mfrow=c(1,2))
+    plot(explore[[var]][,3],final[,1],log="x",xlab=colnames(final)[3],ylab=colnames(final)[1],pch=20,col=alpha(1,.2))
+    plot(final[,3],final[,2],log="x",xlab=colnames(final)[3],ylab=colnames(final)[2],pch=20,col=alpha(1,.2))
+    dev.off()
+}
+
+
+#' Alpha
+#'
+#' A simple function to change the opacity of a color
+#' @param  color the name or idea of a R color
+#' @param  alpha a value \in [0,1] defining the opacity wanted.
+alpha <- function(color,alpha) rgb(t(col2rgb(color)/255),alpha=alpha)
+
+#' Shades
+#'
+#' A simple function to genarte shade of one color by changing its opacity
+#' @param  color the name or idea of a R color
+#' @param  n number or shades wanted
+shades<-function(color,n) sapply(seq(0,1,length.out=n+1),alpha,color=color)[2:(n+1)]
 
