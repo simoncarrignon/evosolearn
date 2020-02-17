2a1c99e50f7ba739e58bf0b0fc3aa581720c4d2e
Simon Carrignon <simon@nimbios.org>
2020-02-17 12:56:56 -0500

    function to automatise plot of equilibrium summary and trajectories

--- toolsSummariesAndTraj.R
+++ toolsSummariesAndTraj.R
@@ -1,66 +1,68 @@
-### barplot version:
 
+plotAllVariableSummaries <- function(summaryresults,E,estimate=NULL,ylim=NULL){
+    Ks=sort(unique(summaryresults$K))
+    mus=sort(unique(summaryresults$mu))
+    ms=sort(unique(summaryresults$m))
+    sigmas=sort(unique(summaryresults$sigma))
+    deltas=sort(unique(summaryresults$delta))
 
-colsK=rev(colorRampPalette(c("red","blue"))(length(Ks)))
+    colsK=rev(colorRampPalette(c("red","blue"))(length(Ks)))
 
-#for(i in folder){
-i="deltas"
-pdf(paste0(i,"_allvariance.pdf"),title=i,width=10,heigh=10,pointsize=12)
-par(mfrow=c(4,2),mar=c(4,4,1,1))
-#sigmascale=c(0.01,0.01,0.01,1.7)
-#names(sigmascale)=sigmas
-allscale=list()
-for(sigma in unique(sumaries$sigma)){
-    allscale[[as.character(sigma)]]=list()
-    for(mu in mus){
-        allsubset=getSubsetWithTraj(allsummaries[["unifPop50k"]],sigma=sigma,mu=mu,m=ms,E=E,K=Ks,traj=F)
-        allmeans=tapply(allsubset$summary$var_x,allsubset$summary[,c("K","m")],mean)
-        allpopMean=tapply(allsubset$summary$N,allsubset$summary[,c("K","m")],mean)
-        allEstimates=allpopMean
-        for(k in as.character(Ks))
-            for(im in as.character(ms))
-                allEstimates[k,im]=eq2833b(allpopMean[k,im],mu,sigma,as.numeric(im))
-        allsds=tapply(allsubset$summary$var_x,allsubset$summary[,c("K","m")],sd)
-        res=barplot(allmeans,beside=T,legend=F,col="white",plot=F)
-        allscale[[as.character(sigma)]][[as.character(mu)]]=c(0,max(allmeans+allsds,allEstimates))
-        ulul=plot(0:max(res),ylim=allscale[[as.character(sigma)]][[as.character(mu)]],type="n",axes=F,xlab="",ylab="")
-        arrows(as.vector(res), allmeans+allsds, as.vector(res), allmeans-allsds,angle=90,code=3,length=.05,lwd=1.5,col=colsK)
-        points(as.vector(res),as.vector(allEstimates),pch=21,bg="white",cex=1)
-        axis(1,at=res[2,],labels=sapply(ms,function(i)as.expression(bquote(m[x] == .(i)))),lwd=0)
+    par(mfrow=c(length(sigmas),length(mus)),mar=c(4,4,1,1))
 
-        axis(2)
-        mtext(bquote(var[x]),2,2,cex=1)
-        mtext(bquote(mu == .(mu) ~ sigma[s] == .(sigma) ),1,3)
-        box()
-        for(id in 1:length(deltas)){
-            d=deltas[id]
-            allsubset=getSubsetWithTraj(sumaries,sigma=sigma,mu=mu,m=ms,E=E,K=Ks,delta=d,traj=F)
-            allmeans=tapply(allsubset$summary$var_x,allsubset$summary[,c("K","m")],mean)
-            allpopMean=tapply(allsubset$summary$N,allsubset$summary[,c("K","m")],mean)
-            allEstimates=allpopMean
-            for(k in as.character(Ks))
-                for(im in as.character(ms))
-                    allEstimates[k,im]=eq2833b(allpopMean[k,im],mu,sigma,as.numeric(im))
-            allsds=tapply(allsubset$summary$var_x,allsubset$summary[,c("K","m")],sd)
-            nres=res+id/10
-            arrows(as.vector(nres), allmeans+allsds, as.vector(nres), allmeans-allsds,angle=90,code=3,length=.05,lwd=1.5,col=colsK,lty=(id+1))
-            points(as.vector(nres),as.vector(allEstimates),pch=21,bg="white",cex=1)
+    for(sigma in sigmas){
+        for(mu in mus){
+            subset=getSubsetWithTraj(summaryresults,sigma=sigma,mu=mu,m=ms,E=E,K=Ks,delta=deltas,traj=F)$summary
+            subdeltas=sort(unique(subset$delta))
+            getallmetrics=lapply(deltas,function(d)
+                                 {
+                                     singlesetup=getSubsetWithTraj(subset,sigma=sigma,mu=mu,m=ms,E=E,K=Ks,delta=d,traj=F)$summary
+                                     if(sum(lengths(singlesetup))==0)return(NULL)
+                                     allmeans=tapply(singlesetup$var_x,singlesetup[,c("K","m")],mean,na.rm=T)
+                                     allsds=tapply(singlesetup$var_x,singlesetup[,c("K","m")],sd,na.rm=T)
+                                     allestimates=NULL
+                                     if(!is.null(estimate)){
+                                         allpopMean=tapply(singlesetup$N,singlesetup[,c("K","m")],mean,na.rm=T)
+                                         allestimates=allpopMean
+                                         for(k in as.character(Ks))
+                                             for(im in as.character(ms))
+                                                 allestimates[k,im]=estimate(allpopMean[k,im],mu,sigma,as.numeric(im))
+                                     }
+                                     allranges=range(allmeans+allsds,allmeans-allsds,allestimates)
+                                     return(list(mean=allmeans,sd=allsds,range=allranges,estimate=allestimates)) 
+                                 }
+            )
+            if(sum(lengths(getallmetrics))==0)break
+            getallmetrics=getallmetrics[lengths(getallmetrics)>0]
+            res=barplot(getallmetrics[[1]]$mean,beside=T,legend=F,col="white",plot=F)
+            ranges=range(lapply(getallmetrics,"[[","range"),na.rm=T)
+            plot(0:max(res),ylim=ranges,type="n",axes=F,xlab="",ylab="")
+            axis(1,at=res[2,],labels=sapply(ms,function(i)as.expression(bquote(m[x] == .(i)))),lwd=0)
+
+            axis(2)
+            mtext(bquote(var[x]),2,2,cex=1)
+            mtext(bquote(mu == .(mu) ~ sigma[s] == .(sigma) ),1,3)
+            box()
+            for(id in 1:length(getallmetrics)){
+                d=getallmetrics[[id]]
+                nres=res+id/10
+                if(!is.null(estimate)) points(as.vector(nres),as.vector(d$estimate),pch=21,bg="white",cex=1)
+                arrows(as.vector(nres), d$mean+d$sd, as.vector(nres), d$mean-d$sd,angle=90,code=3,length=.01,lwd=1.5,col=colsK,lty=id)
+            }
+        legend("topleft",
+               legend=c(paste0("K=",Ks),"Hermisson",sapply(subdeltas,function(d)as.expression(bquote(delta==.(d))))),
+               col=c(colsK,1,rep(1,length(subdeltas))),
+               lty=c(rep(1,length(Ks)),NA,seq_along(subdeltas)),
+               pch=c(rep(NA,length(Ks)),21,rep(NA,length(subdeltas))),
+               )
         }
     }
-    legend("topleft",
-           legend=c(paste0("K=",Ks),"Hermisson",sapply(c(0,deltas),function(d)as.expression(bquote(delta==.(d))))),
-           col=c(colsK,1,rep(1,length(deltas)+1)),
-           lty=c(rep(1,length(Ks)),NA,1,seq_along(deltas)+1),
-           pch=c(rep(NA,length(Ks)),21,rep(NA,length(deltas)+1)),
-           )
-}
-dev.off()
 
+}
 
 
 #this function use the filename stored in an summarized ouput  to get the individual trajectory for each experimetn an trace them.
 plotAllTrajVar <- function(summaryresults,obs="N",m,E,ylim=NULL,legside="topleft"){
-
     Ks=sort(unique(summaryresults$K))
     mus=sort(unique(summaryresults$mu))
     ms=sort(unique(summaryresults$m))
@@ -112,7 +114,6 @@ plotAllTrajVar <- function(summaryresults,obs="N",m,E,ylim=NULL,legside="topleft
 
 ## this funciton take all experiment from one or more folders and return it as a unique dataframe
 getAlllSummaries <- function(folder){
-    folder=c("omega1growingDelta")
     allsummaries=lapply(folder,getFullExperimentSummary)
     names(allsummaries)=folder
     allexperiments=lapply(allsummaries,function(curr)
