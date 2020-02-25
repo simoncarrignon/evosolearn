12dee72963105aa43f44c7fa762cd5385b9728cb
Simon Carrignon <simon@nimbios.org>
2020-02-25 15:21:27 -0500

    correct various bug and simplyf the group plot

--- toolsSummariesAndTraj.R
+++ toolsSummariesAndTraj.R
@@ -5,10 +5,11 @@ plotAllVariableSummaries <- function(summaryresults,E,estimate=NULL,ylim=NULL){
     ms=sort(unique(summaryresults$m))
     sigmas=sort(unique(summaryresults$sigma))
     deltas=sort(unique(summaryresults$delta))
-
+    omegas=sort(unique(summaryresults$omega))
+    if(is.null(omegas))omega=0
     colsK=rev(colorRampPalette(c("red","blue"))(length(Ks)))
 
-    par(mfrow=c(length(sigmas),length(mus)),mar=c(4,4,1,1))
+    par(mfrow=c(length(sigmas),length(mus)),mar=c(3,4,2,0))
 
     for(sigma in sigmas){
         for(mu in mus){
@@ -36,12 +37,13 @@ plotAllVariableSummaries <- function(summaryresults,E,estimate=NULL,ylim=NULL){
             getallmetrics=getallmetrics[lengths(getallmetrics)>0]
             res=barplot(getallmetrics[[1]]$mean,beside=T,legend=F,col="white",plot=F)
             ranges=range(lapply(getallmetrics,"[[","range"),na.rm=T)
+            if(any(is.infinite(ranges)))ranges=c(-1,1)
             plot(0:max(res),ylim=ranges,type="n",axes=F,xlab="",ylab="")
             axis(1,at=res[2,],labels=sapply(ms,function(i)as.expression(bquote(m[x] == .(i)))),lwd=0)
 
             axis(2)
             mtext(bquote(var[x]),2,2,cex=1)
-            mtext(bquote(mu == .(mu) ~ sigma[s] == .(sigma) ),1,3)
+            mtext(bquote(mu == .(mu) ~ sigma[s] == .(sigma) ),3,0)
             box()
             for(id in 1:length(getallmetrics)){
                 d=getallmetrics[[id]]
@@ -68,43 +70,64 @@ plotAllTrajVar <- function(summaryresults,obs="N",m,E,ylim=NULL,legside="topleft
     ms=sort(unique(summaryresults$m))
     sigmas=sort(unique(summaryresults$sigma))
     deltas=sort(unique(summaryresults$delta))
+    omega=sort(unique(summaryresults$omega))
+    if(is.null(omega))omega=0
 
     par(mfrow=c(length(sigmas),length(mus)))
+
+    #different colos will represent different Ks
+    cols=colorRampPalette(c("red","blue"))(length(Ks))
+    names(cols)=Ks
+
     for(sigma in sigmas){
         for(mu in mus){
 
-            subsetraj=getSubsetWithTraj(summaryresults,sigma=sigma,mu=mu,m=ms,E=E,K=Ks,delta=deltas,var=obs,traj=T)
+            subsetraj=getSubsetWithTraj(summaryresults,sigma=sigma,mu=mu,m=m,E=E,K=Ks,delta=deltas,var=obs,traj=T)
+            param=bquote(sigma[s] == .(sigma) ~ E == .(E) ~ m == .(m) ~ mu == .(mu) ~ omega == .(omega) )
+
+            if(is.null(dim(subsetraj$traj))){#some simulation have differen lenght
+                minlen=min(lengths(subsetraj$traj))
+                subsetraj$traj=sapply(subsetraj$traj,function(i){
+                            if(length(i)>minlen){
+                                rescale=seq(1,length(i),length.out=minlen)
+                                return(i[rescale])
+                            }
+                            return(i)
+               })
+            }
+
+            a=apply(subsetraj$traj,1,function(l)quantile(l[(length(l)/2):length(l)],na.rm=T))
             maxts_n=max(subsetraj$summary$extinction) # get real scale of observation
 
-            if(is.null(ylim))ylim=range(subsetraj$traj,na.rm=T)
-            param=bquote("full traj" ~ .(obs) ~ "with" ~ sigma[s] == .(sigma) ~ E == .(E) ~ m == .(m) ~ mu == .(mu) )
+            if(is.null(ylim))yl=range(a,na.rm=T)
+            else yl =ylim
+            if(any(is.infinite(yl)))yl=c(-1,1)
+
+            #param=bquote("full traj" ~ .(obs) ~ "with" ~ sigma[s] == .(sigma) ~ E == .(E) ~ m == .(m) ~ mu == .(mu) )
 
             #prepare main plot area
-            plot(1,1,xlab="time",ylab=obs,ylim=ylim,xlim=c(1,maxts_n),type="n",main=param)
-            legend(legside,
-                   legend=c(paste0("K=",Ks),sapply(deltas,function(d)as.expression(bquote(delta==.(d))))),
-                   col=c(cols,rep(length(deltas))),
-                   lty=c(rep(1,length(Ks)),seq_along(deltas))
-                   )
 
+            plot(1,1,xlab="time",ylab=obs,ylim=yl,xlim=c(1,maxts_n),type="n",main=param)
             for(id in 1:length(deltas)){
                 d=deltas[id]
 
                 singlesetup=lapply(Ks,function(k)getSubsetWithTraj(subsetraj$summary,sigma=sigma,mu=mu,m=m,E=E,K=k,delta=d,var=obs,traj=T)$traj)
 
                 if(sum(lengths(singlesetup))==0)break
-                #adjust length of single setup to ouptu
+                #adjust length of single setup to ouptut
                 maxts=nrow(singlesetup[[1]])
                 rangesummaries=seq(1,maxts_n,length.out=maxts)
 
-                a=sapply(singlesetup,quantile,na.rm=T)[c(2,4),]
-                cols=colorRampPalette(c("red","blue"))(length(singlesetup))
-                names(cols)=allsubmu
-                lapply(1:length(allsubmu),function(sub)
+                lapply(1:length(Ks),function(sub)
                        {
                            plotTraj(rangesummaries,singlesetup[[sub]],col=cols[sub],xlim=range(rangesummaries),add=T,mean=T,lty=id,lwd=.8)
                        })
             }
+            legend(legside,
+                   legend=c(paste0("K=",Ks),sapply(deltas,function(d)as.expression(bquote(delta==.(d))))),
+                   col=c(cols,rep(1,length(deltas))),
+                   lty=c(rep(1,length(Ks)),seq_along(deltas))
+                   )
         }
 
     }
@@ -113,8 +136,8 @@ plotAllTrajVar <- function(summaryresults,obs="N",m,E,ylim=NULL,legside="topleft
 
 
 ## this funciton take all experiment from one or more folders and return it as a unique dataframe
-getAlllSummaries <- function(folder){
-    allsummaries=lapply(folder,getFullExperimentSummary)
+getAlllSummaries <- function(folder,exclude=NULL){
+    allsummaries=lapply(folder,getFullExperimentSummary,exclude=exclude)
     names(allsummaries)=folder
     allexperiments=lapply(allsummaries,function(curr)
                           {
