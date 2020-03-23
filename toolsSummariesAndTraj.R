07135dcd8eed185fe95ece6fe93bc1f04179d31d
Simon <simon@elisya.org>
2020-03-23 11:46:32 +0100

    new tool to plot dist and fitness and removed null estimates

--- toolsSummariesAndTraj.R
+++ toolsSummariesAndTraj.R
@@ -42,8 +42,11 @@ plotAllVariableSummaries <- function(summaryresults,E,estimate=NULL,ylim=NULL,va
             if(sum(lengths(getallmetrics))==0)break
             getallmetrics=getallmetrics[lengths(getallmetrics)>0]
             res=barplot(getallmetrics[[1]]$mean,beside=T,legend=F,col="white",plot=F)
+            if(is.null(ylim)){
             ranges=range(lapply(getallmetrics,"[[","range"),na.rm=T)
             if(any(is.infinite(ranges)))ranges=c(-1,1)
+            }
+            else{ ranges=ylim}
             plot(0:max(res),ylim=ranges,type="n",axes=F,xlab="",ylab="")
             axis(1,at=res[2,],labels=sapply(ms,function(i)as.expression(bquote(m[x] == .(i)))),lwd=0)
 
@@ -54,8 +57,10 @@ plotAllVariableSummaries <- function(summaryresults,E,estimate=NULL,ylim=NULL,va
             for(id in 1:length(getallmetrics)){
                 d=getallmetrics[[id]]
                 nres=res+id/10
-                if(!is.null(estimate)) points(as.vector(nres),as.vector(d$estimate),pch=21,bg="white",cex=1)
-                if(sigma>1000) points(as.vector(nres),as.vector(d$noselection),pch=22,bg="white",cex=1)
+                if(!is.null(estimate)){
+                    points(as.vector(nres),as.vector(d$estimate),pch=21,bg="white",cex=1)
+                    if(sigma>1000) points(as.vector(nres),as.vector(d$noselection),pch=22,bg="white",cex=1)
+                }
                 arrows(as.vector(nres), d$mean+d$sd, as.vector(nres), d$mean-d$sd,angle=90,code=3,length=.01,lwd=1.5,col=colsK,lty=id)
             }
             if(!is.null(estimate)){
@@ -181,4 +186,67 @@ updateScale <- function(df.exp){
     df.exp$extinction=(df.exp$extinction-1)*unique(df.exp$outputrate)+1
     return(df.exp)
 }
+
+
+#plot tow lines: distance to optimum and fintes
+plotDistVsFitness <- function(summaryresults,ylim=NULL,legside="topleft"){
+    Ks=sort(unique(summaryresults$K))
+    mus=sort(unique(summaryresults$mu))
+    ms=sort(unique(summaryresults$m))
+    sigmas=sort(unique(summaryresults$sigma))
+    deltas=sort(unique(summaryresults$delta))
+    omega=sort(unique(summaryresults$omega))
+
+            subsetraj=getSubsetWithTraj(summaryresults,sigma=sigmas,mu=mus,m=ms,E=0,K=Ks,delta=deltas,var="dist",traj=T)
+            param=bquote(sigma[s] == .(sigma) ~ E == .(E) ~ m == .(m) ~ mu == .(mu) ~ omega == .(omega) )
+
+            if(is.null(dim(subsetraj$traj))){#some simulation have differen lenght
+                minlen=min(lengths(subsetraj$traj))
+                subsetraj$traj=sapply(subsetraj$traj,function(i){
+                            if(length(i)>minlen){
+                                rescale=seq(1,length(i),length.out=minlen)
+                                return(i[rescale])
+                            }
+                            return(i)
+               })
+            }
+
+            a=apply(subsetraj$traj,1,function(l)quantile(l[(length(l)/2):length(l)],na.rm=T))
+            maxts_n=max(subsetraj$summary$extinction) # get real scale of observation
+
+            if(is.null(ylim))yl=range(a,na.rm=T)
+            else yl =ylim
+            if(any(is.infinite(yl)))yl=c(-1,1)
+
+            #param=bquote("full traj" ~ .(obs) ~ "with" ~ sigma[s] == .(sigma) ~ E == .(E) ~ m == .(m) ~ mu == .(mu) )
+
+            #prepare main plot area
+
+            plot(1,1,xlab="time",ylab=obs,ylim=yl,xlim=c(1,maxts_n),type="n",main=param)
+            for(id in 1:length(deltas)){
+                d=deltas[id]
+
+                singlesetup=lapply(Ks,function(k)getSubsetWithTraj(subsetraj$summary,sigma=sigma,mu=mu,m=m,E=E,K=k,delta=d,var=obs,traj=T)$traj)
+
+                if(sum(lengths(singlesetup))==0)break
+                #adjust length of single setup to ouptut
+                maxts=nrow(singlesetup[[1]])
+                rangesummaries=seq(1,maxts_n,length.out=maxts)
+
+                lapply(1:length(Ks),function(sub)
+                       {
+                           plotTraj(rangesummaries,singlesetup[[sub]],col=cols[sub],xlim=range(rangesummaries),add=T,mean=T,lty=id,lwd=.8)
+                       })
+            }
+            legend(legside,
+                   legend=c(paste0("K=",Ks),sapply(deltas,function(d)as.expression(bquote(delta==.(d))))),
+                   col=c(cols,rep(1,length(deltas))),
+                   lty=c(rep(1,length(Ks)),seq_along(deltas))
+                   )
+        }
+
+    }
+
+}
+
 
