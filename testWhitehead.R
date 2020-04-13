4f1bb7de6923cf67828e8a1be413034779c12d52
Simon Carrignon <simon@nimbios.org>
2020-04-13 03:47:30 -0400

    explore W07 in details

--- testWhitehead.R
+++ testWhitehead.R
@@ -6,21 +6,26 @@ getFirst <- function(allpopres){
     }
     return(ctsls)
 }
+
+
 q=0.8494 #sigma_s
 
 delta=2^seq(-3,3)
 
 omega=seq(-.5,2.5,.5)
 
-E_x=c(0.08,0.15,.25,.50,.75) #sigma for whitehead, E[x] pour nous
-C_v=1-c(0.01,0.03,0.05,0.07,0.1)
-C_h=C_v-c(0.01,0.03,0.05,0.07,0.1)
-C_i=C_h-c(0.01,0.03,0.05,0.07,0.1)
+sigma=c(0.08,0.25,.75) #sigma for whitead=>accuracy of behavior (E) for us
+E_x=c(0.08,0.15,.25,.50,.75) #sigma for whitehead, E for us
+C_v=1-c(0.01,0.03,0.05,0.07,0.1) #cost vertical learning
+C_h=C_v-c(0.01,0.03,0.05,0.07,0.1)#cost horizont learning
+C_i=C_h-c(0.01,0.03,0.05,0.07,0.1)#cost individual learning
+
+expand.grid(sigma=E_x,C_v=C_v,C_i=C_i)
 
 n=100
 pop=generatePop(n,distrib=list(x=rep(0,n),y=rep(0,n),z=rep(0,n)))
 pop=cbind(pop,sls=-1)  #sls -1 => genetic
-sigma=0.08
+
 pop[,"x"]=rnorm(n,0,sigma)
 pop[26:50,"y"]=1
 pop[26:50,"sls"]=0#sls 0 =>individual 
@@ -33,14 +38,14 @@ tstep=15000
 K=1500
 
 library(parallel)
-cl <- makeForkCluster(4,outfile="")
+cl <- makeForkCluster(16,outfile="")
 allresults=lapply(1:7,function(d)
                   {
                       lapply(1:7,function(o)
                              {
                                  print(paste(o,d))
                                  env=environment(N=tstep,omega=omega[o],delta=delta[d])
-                                 parLapply(cl,1:50,function(i)
+                                 parLapply(cl,1:100,function(i)
                                         {
                                             print(i);
                                             result=getFirst(simpleEvoModel(n,tstep,omega = 0,delta = 0 ,b=2,K=K,mu=c(x=0,y=0,z=0),m=c(x=0,y=0,z=0),E=c(x=0,y=0,z=sigma),sigma=c(s=exp(1),y=exp(C_i[1]),z=exp(C_v[1])),log=F,sls="mixed",pop=pop,outputrate=1,allpops=T,theta=env))
