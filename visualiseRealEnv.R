bdf38a8167f644ba2974306fae3a47d2fd72b853
Simon <simon@elisya.org>
2020-05-18 12:03:40 +0200

    send function to toolls pool of function

--- visualiseRealEnv.R
+++ visualiseRealEnv.R
@@ -1,38 +1,15 @@
 slpalette=colorRampPalette(c(rgb(0,.5,0),rgb(0,.5,1)))
 ilpalette=colorRampPalette(c(rgb(0,.5,0),rgb(1,.5,0)))
 survivalpallette=colorRampPalette(c("grey","yellow","dark green"))
-extinctpalette=colorRampPalette(c("chartreuse","white"))
+extinctpalette=colorRampPalette(c("chartreuse4","white"))
 pureSl=slpalette(2)[2]
 pureIl=ilpalette(2)[2]
 
-environment=read.csv("data/theta_real.csv")
-binded=do.call("rbind",lapply(list.files(path="exploreRealEnvBESTfull",recursive=T,full.names=T,pattern="*cross*"),function(u){print(u);load(u);return(binded)}))
-
-plotMatrixStrateAndEn <- function(sum_mat,environment){
-    pureIl=rgb(1,.5,0)
-    pureSl=rgb(0,.5,1)
-    mixed=rgb(1,.5,1)
-    par(fig=c(0,1,.8,1),mar=rep(0,4),oma=c(2,2,1,1),new=T)
-    plot(environment$permille,col="dark green",lwd=1,type="l",axes=F)
-    par(new=T)
-    plotTres(sum_mat$mean_x,ylim=range(sum_mat$mean_x))
-    mtext(expression(theta),4,0,col="dark green")
-    mtext(expression(bar(x)),2,0,col="1")
-    par(fig=c(0,1,0,.8),new=T)
-    plotTres(sum_mat$prop_y,col=pureIl,ylim=c(0,1))
-    par(new=T)
-    plotTres(sum_mat$prop_z,col=pureSl,ylim=c(0,1))
-    par(new=T)
-    plotTres(sum_mat$prop_yz,col=mixed,ylim=c(0,1))
-    axis(1);axis(2);box()
-}
-
-
-plotTres <- function(u,col=1,...){
-    plot(u[2,],col=col,axes=F,type="l",...)
-    lines(u[1,],col=col,lty=2)
-    lines(u[3,],col=col,lty=2)
-}
+realdata=read.csv("data/41586_2004_BFnature02805_MOESM1_ESM.csv")
+theta=rev(tapply(realdata$permille,realdata$years.BP.2000,mean))
+plot(theta)
+binded=do.call("rbind",lapply(list.files(path="exploreRealEnvBESTsigMas",recursive=T,full.names=T,pattern="*cross*"),function(u){print(u);load(u);return(binded)}))
+binded=binded[binded$sigma==2,]
 
 
 for(s in unique(binded$sigma)){
@@ -48,7 +25,7 @@ for(s in unique(binded$sigma)){
                     nexpe=nrow(subb)
                     vars=c("prop_y","prop_z","mean_x","N","prop_yz")
                     names(vars)=vars
-                    mat_allexp= lapply(vars,function(i)matrix(NA,nexpe,4918))
+                    mat_allexp= lapply(vars,function(i)matrix(NA,nexpe,length(theta)))
 
                     for(i in 1:50){
                         load(as.character(subb$filename[i]))
@@ -57,7 +34,7 @@ for(s in unique(binded$sigma)){
                     sum_mat=lapply(mat_allexp,function(m)apply(m,2,quantile,probs=c(.05,.5,.95),na.rm=T))
                     fname=paste0("traj_s",s,"_m",m,"_mu",mu,"_ky",ky,"_kz",kz,".png")
                     #png(fname,width=450,height=450,pointsize=28)
-                    plotMatrixStrateAndEn(sum_mat,environment)
+                    plotMatrixStrateAndEn(sum_mat,theta)
                     #dev.off()
                 }
             }
@@ -161,13 +138,15 @@ dev.off()
 png(paste0("exploringRealEnv.png"),width=450,height=500,pointsize=16)
 
 par(mfrow=c(nlines,ncols),oma=c(6,5,2,5))
+nexp=c()
 for(s in unique(binded$sigma)){
-    for(kz in unique(binded$k_z)){
-        for(sg in unique(binded$k_y)){
-            subb=binded[binded$sigma ==s & binded$k_z ==kz & binded$k_y ==sg,]
+    for(kz in rev(sort(unique(binded$k_z)))){
+        for(ky in unique(binded$k_y)){
+            subb=binded[binded$sigma ==s & binded$k_z ==kz & binded$k_y ==ky,]
             par(mar=rep(0,4))
             res=tapply(subb$mean_y,subb[,c("mu","m")],function(i)sum(is.na(i)))
-            image(t(res),xaxt="n",yaxt="n",ylab="",xlab="",zlim=range(0,50),col=extinctpalette(50))
+            nexp=min(tapply(subb$mean_y,subb[,c("mu","m")],length))
+            image(t(res),xaxt="n",yaxt="n",ylab="",xlab="",zlim=range(0,nexp),col=extinctpalette(nexp))
         }
     }
 }
@@ -185,8 +164,8 @@ kz=sapply(unique(binded$k_z),function(u)as.expression(bquote(k[z] == .(u))))
 mtext(kz,side=2,line=2,at=seq(.5/length(kz),2.5/length(kz),length.out=length(kz)),cex=.9,outer=T)
 
 par(xpd=NA)
-points(rep(1.5,8),seq(3,2.8,length.out=8),cex=2,pch=22,col=NA,bg=rev(extinctpalette(8)))
-text(rep(1.8,3),seq(3,2.8,length.out=3),paste0(round(seq(max(binded$N,na.rm=T),0,length.out=3),digits=2)),cex=1.2,adj=0)
+points(rep(1.5,8),seq(3,2.8,length.out=8),cex=2,pch=22,col=NA,bg=rev(extinctpalette(nexp)))
+text(rep(1.8,3),seq(3,2.8,length.out=3),paste0(round(seq(0,nexp,length.out=3),digits=2)),cex=1.2,adj=0)
 text(1.8,3.11,expression(bar(N)[e]),cex=1.2,adj=0)
 
 dev.off()
