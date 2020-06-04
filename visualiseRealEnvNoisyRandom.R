66762518a2fa8bc63dd674dd947ea4c535a8bc6b
Simon Carrignon <simon@nimbios.org>
2020-06-04 06:26:45 -0400

    update visualisation toolls

--- visualiseRealEnvNoisyRandom.R
+++ visualiseRealEnvNoisyRandom.R
@@ -1,26 +1,32 @@
 source("environment.R")
-setAxis <- function(){
-    mus=sapply(unique(binded$mu),function(u)as.expression(bquote(mu == .(u))))
+setAxis <- function(data){
+    mus=sapply(unique(data$mu),function(u)as.expression(bquote(mu == .(u))))
     tm=5
     nl=seq(1,length(mus),length.out=tm)
     axis(1,label=mus[nl],at=seq(0,1,length.out=length(nl)),cex.axis=1.1,las=3)
 
-    ms=paste0("m=",unique(binded$m))
+    ms=paste0("m=",unique(data$m))
     tm=4
     nl=seq(1,length(ms),length.out=tm)
     axis(4,label=ms[nl],at=seq(0,1,length.out=length(nl)),las=1,cex.axis=1.1)
 
-    e=sapply(unique(binded$E),function(u)as.expression(bquote(E == .(u))))
+    e=sapply(unique(data$E),function(u)as.expression(bquote(E == .(u))))
     mtext(e,side=3,line=0,at=seq(.5/length(e),3.5/length(e),length.out=length(e)),cex=.9,outer=T)
 
     vsp=1/nlines
 
-    ky=sapply(unique(binded$k_y),function(u)as.expression(bquote(k[y] == .(u))))
+    ky=sapply(unique(data$k_y),function(u)as.expression(bquote(k[y] == .(u))))
     mtext(ky,side=2,line=2,at=seq(1.5*vsp,1-1.5*vsp,length.out=length(ky)),cex=.9,outer=T)
 
     vsp=1/nlines
-    kz=sapply(unique(binded$k_z),function(u)as.expression(bquote(k[z] == .(u))))
+    kz=sapply(unique(data$k_z),function(u)as.expression(bquote(k[z] == .(u))))
     mtext(kz,side=2,line=0,at=seq(vsp-.5*vsp,length(kz)*vsp-.5*vsp,length.out=length(kz)),cex=.9,outer=T)
+}
+
+plotVertEnv <- function(realdata,scale,...){
+    plot(rev((realdata$dTsVscales)*scale),rev(realdata$year),type="l",yaxs="i",axes=F,...)
+    plot(c(0, 1), c(0, 1), type = "n",xlim=c(0,1),ylim=c(0,1),axes=F,xlab="",ylab="",xaxs="i",yaxs="i")
+    axis(2,label=rev(round(sort(seq(0,min(realdata$year),length.out=5)*scale))),at=seq(0,1,length.out=5),outer=T)
 }
 
 slpalette=colorRampPalette(c(rgb(1,.5,0),rgb(0,.5,1)))
@@ -30,12 +36,9 @@ extinctpalette=colorRampPalette(c("chartreuse4","white"))
 pureSl=slpalette(2)[2]
 pureIl=ilpalette(2)[2]
 
-realdata=read.csv("data/theta_real.csv")
-newt=interpolate(realdata$permille,realdata$years.BP.2000,finalres=.5)
-#sub=seq(1,length(newt),length.out=length(newt)/5)
-theta=rev(-newt)
 
-
+namesRealEnv=c("epica","lr04","ls16","vostok")
+namesFun=c("getMean","getLast","getFirst")
 allexp=list(
 #list(folder="exploreRealEnvRANDOMNoisy",
 #idexpe="stackLR04InterpolLinear")
@@ -43,9 +46,12 @@ allexp=list(
 #list(folder="exploreRealEnvRANDOMNoisyHigherRes",
 #idexpe="stackLR04InterpolNoisy")
 #,
-list(folder="exploreRealEnvRANDOMNoisyHigherResHighSigma",
-idexpe="stackLR04InterpolNoisyRescale")
+list(folder="epicagetLast",
+idexpe="epicagetLast")
 )
+
+combiname=expand.grid(namesRealEnv,namesFun)
+allexp=apply(combiname,1,function(i)list(folder=paste0(i,collapse=""),idexpe=paste0(i,collapse=""),env=i[1],fun=i[2]))
 
 for( exp in allexp){
 
@@ -81,7 +87,6 @@ for(s in unique(binded2$sigma)){
                 pxl[,]=NA
                 for(i in 1:nrow(resy)){
                     for(j in 1:ncol(resy)){
-                        print(paste(i,j))
                         if(is.nan(resy[i,j]))pxl[i,j]=NA
                         else
                             pxl[i,j]=rgb(red=resy[i,j],green=.5,blue=resz[i,j])
@@ -94,7 +99,7 @@ for(s in unique(binded2$sigma)){
             }
         }
     }
-    setAxis()
+    setAxis(binded)
     mtext("mean genotype",1,2,at=0.1,outer=T,adj=0,cex=1.2)
 
     dev.off()
@@ -114,7 +119,7 @@ for(s in unique(binded2$sigma)){
             }
         }
     }
-    setAxis()
+    setAxis(binded)
     par(xpd=NA)
     top=10
     points(rep(1.3,8),seq(top,top-.5,length.out=8),cex=2,pch=22,col=NA,bg=rev(survivalpallette(8)))
@@ -141,7 +146,7 @@ for(s in unique(binded2$sigma)){
             }
         }
     }
-    setAxis()
+    setAxis(binded)
     par(xpd=NA)
     points(rep(1.3,8),seq(top,top-.5,length.out=8),cex=2,pch=22,col=NA,bg=extinctpalette(8))
     text(rep(1.5,3),seq(top,top-.5,length.out=3),paste0(round(seq(0,nexp,length.out=3))),cex=1.2,adj=0)
@@ -180,27 +185,36 @@ for(s in unique(binded2$sigma)){
                     mat_allexp= lapply(vars,function(i)matrix(NA,nexpe,tsteps))
                     for(i in 1:nexpe){
                         load(as.character(subb$filename[i]))
-                        for(v  in vars) mat_allexp[[v]][i,]=summary[,v]
+                        for(v  in vars){
+                            if(length(mat_allexp[[v]][i,])!=length(summary[,v]))
+                                print(i)
+                            else
+                                mat_allexp[[v]][i,]=summary[,v]
+
+                        }
                     }
                     sum_mat=lapply(mat_allexp,function(m)apply(m,2,mean,na.rm=T))
                     sum_mat$na=apply(mat_allexp$mean_y,2,function(i)sum(is.na(i)))
                     sum_mat=lapply(sum_mat,function(m)m[!is.na(m)])
                     if(length(sum_mat$mean_y)>1){
-                        pxl=rgb(sum_mat$mean_y,.5,sum_mat$mean_z,alpha=1-sum_mat$na/nexpe)
+                        if(is.na(sum_mat$na[1]))
+                            pxl=NA
+                        else
+                            pxl=rgb(sum_mat$mean_y,.5,sum_mat$mean_z,alpha=1-sum_mat$na/nexpe)
                         bicpic[1:length(pxl),p]=pxl
                     }
 
                     p=p+1
                     print(paste("col",p,"/",ncol(bicpic)))
                 }
-                scale=1000 #to convert time scale in year
+                scale=1 #to convert time scale in year
                 png(paste0("images/vertical_",idexpe,"_sigma",s,"_E",e,".png"),width=600)
                 par(mar=rep(0,4),oma=c(1,4,4,1))
                 prop=.1
                 layout(matrix(c(1,2),ncol=2,nrow=1),width=c(prop,1-prop))
-                plot(rev(realdata$permille),rev(realdata$years.BP.2000)*scale,type="l",yaxs="i",axes=F)
-                plot(c(0, 1), c(0, 1), type = "n",xlim=c(0,1),ylim=c(0,1),axes=F,xlab="",ylab="",xaxs="i",yaxs="i")
-                axis(2,label=seq(0,max(realdata$years.BP.2000),length.out=5)*scale,at=seq(0,1,length.out=5),outer=T)
+                env=exp$env
+                env=read.csv(paste0("data/",exp$env,".csv"))
+                plotVertEnv(env,scale)
                 rasterImage(bicpic, 0, 0, 1, 1,interpolate=F)
                 mtext("year BP",3,2,at=0,outer=T)
                 mtext(expression(delta[18]*O),3,0,at=prop/2,outer=T)
@@ -219,7 +233,7 @@ for(s in unique(binded2$sigma)){
 }
 
 for(s in unique(binded2$sigma)){
-    subnum=5
+    subnum=20
 
     binded=binded2[binded2$sigma==s,]
     mum=as.data.frame(expand.grid(m=unique(binded$m),mu=unique(binded$mu)))
@@ -245,22 +259,22 @@ for(s in unique(binded2$sigma)){
                         pxl=c()
                         load(as.character(subb$filename[i]))
                         na=which.max(is.na(summary[,1]))#find the first na ie when pop get extinct
-                        if(na>1) summary=summary[1:(na-1),]
-                        pxl=rgb(summary[,"mean_y" ],.5,summary[,"mean_z"],alpha=1-min(1,summary[,"N"]/1000))
+                        if(na>1) summary=summary[1:(na-1),,drop=F]
+                        if(na==1)pxl=NA
+                        else
+                            pxl=rgb(summary[,"mean_y" ],.5,summary[,"mean_z"],alpha=.5+1/2*min(1,summary[,"N"]/1000))
                         bicpic[1:length(pxl),p]=pxl
                         p=p+1
-                        print(paste("exp",as.character(subb$filename[i])))
                         print(paste("col",p,"/",ncol(bicpic)))
                     }
                 }
                 scale=1000 #to convert time scale in year
-                png(paste0("images/vertical_",idexpe,"_sigma",s,"_E",e,".png"),width=600)
+                png(paste0("images/verticalM_",idexpe,"_sigma",s,"_E",e,".png"),width=1800,height=1000,pointsize=42)
                 par(mar=rep(0,4),oma=c(1,4,4,1))
                 prop=.1
                 layout(matrix(c(1,2),ncol=2,nrow=1),width=c(prop,1-prop))
-                plot(rev(realdata$permille),rev(realdata$years.BP.2000)*scale,type="l",yaxs="i",axes=F)
-                plot(c(0, 1), c(0, 1), type = "n",xlim=c(0,1),ylim=c(0,1),axes=F,xlab="",ylab="",xaxs="i",yaxs="i")
-                axis(2,label=seq(0,max(realdata$years.BP.2000),length.out=5)*scale,at=seq(0,1,length.out=5),outer=T)
+                env=read.csv(paste0("data/",exp$env,".csv"))
+                plotVertEnv(env,scale,lwd=4)
                 rasterImage(bicpic, 0, 0, 1, 1,interpolate=F)
                 mtext("year BP",3,2,at=0,outer=T)
                 mtext(expression(delta[18]*O),3,0,at=prop/2,outer=T)
